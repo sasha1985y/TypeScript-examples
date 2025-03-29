92eda17a4ca732d5ac4c935fa215791325b65c7b
sasha1985y <kuyancev1985@mail.ru>
2025-03-30 01:42:46 +0300

    try deploy_14

--- webpack.config.js
+++ webpack.config.js
@@ -24,7 +24,7 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true, // Очищать папку dist перед каждой сборкой
-    publicPath: '/', // Добавьте это
+    publicPath: './dist/index.html', // Добавьте это
   },
   devtool: 'source-map', // Для генерации sourcemaps
   devServer: {
